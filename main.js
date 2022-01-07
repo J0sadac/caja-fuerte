9162438afc7ef2fbccb5d80f14f28884444c2a16
Josadac Castañeda <jugdmentc12@gmail.com>
2022-01-07 01:05:54 -0600

    termine la base del sitio, version 0.1.2

--- main.js
+++ main.js
@@ -1,11 +1,54 @@
-'use strict'
+$(document).ready(function(){
+    var contenido = $(".contenido");
+    var opciones = $(".opciones");
 
-var opciones = document.getElementById("option");
+    opciones.hide();
+  
+    $("#titulo-01").click(function(){
+        opciones.css("border-top", "none");
+        $("#opciones-01").fadeToggle();
+    });
 
-function desplegar(click){
-    if(click == true){
-        opciones == display;
+    $("#titulo-02").click(function(){
+        opciones.css("border-top", "none");
+        $("#opciones-02").fadeToggle();
+    });
 
-    }
+    $("#titulo-03").click(function(){
+        opciones.css("border-top", "none");
+        $("#opciones-03").fadeToggle();
+    });
 
-}
+    $("#titulo-04").click(function(){
+        opciones.css("border-top", "none");
+        $("#opciones-04").fadeToggle();
+    });
+
+    $("#titulo-05").click(function(){
+        opciones.css("border-top", "none");
+        $("#opciones-05").fadeToggle();
+    });
+
+    $("#titulo-06").click(function(){
+        opciones.css("border-top", "none");
+        $("#opciones-06").fadeToggle();
+    });
+
+    $("#titulo-07").click(function(){
+        opciones.css("border-top", "none");
+        $("#opciones-07").fadeToggle();
+    });
+
+    $("#titulo-08").click(function(){
+        opciones.css("border-top", "none");
+        $("#opciones-08").fadeToggle();
+    });
+
+    $("#titulo-09").click(function(){
+        opciones.css("border-top", "none");
+        $("#opciones-09").fadeToggle();
+    });
+
+})
+
+
