1fd7eb3d65c64500e619aac5e6d8ccdd80cd0fd8
Josadac Castañeda <jugdmentc12@gmail.com>
2022-01-07 20:50:18 -0600

    hice el cambio de color a los titulos, version 0.1.3

--- main.js
+++ main.js
@@ -8,46 +8,154 @@ $(document).ready(function(){
         opciones.css("border-top", "none");
         $("#opciones-01").fadeToggle();
     });
+    $("#listo-01").click(function(){
+        $("#titulo-01").css("background-color", "green");
+        opciones.hide();
+    });
+    $("#noListo-01").click(function(){
+        $("#titulo-01").css("background-color", "red");
+        opciones.hide();
+    });
+    $("#actualizar-01").click(function(){
+        $("#titulo-01").css("background-color", "blue");
+    });
+
 
     $("#titulo-02").click(function(){
         opciones.css("border-top", "none");
         $("#opciones-02").fadeToggle();
     });
+    $("#listo-02").click(function(){
+        $("#titulo-02").css("background-color", "green");
+        opciones.hide();
+    });
+    $("#noListo-02").click(function(){
+        $("#titulo-02").css("background-color", "red");
+        opciones.hide();
+    });
+    $("#actualizar-02").click(function(){
+        $("#titulo-02").css("background-color", "blue");
+    });
+
 
     $("#titulo-03").click(function(){
         opciones.css("border-top", "none");
         $("#opciones-03").fadeToggle();
     });
+    $("#listo-03").click(function(){
+        $("#titulo-03").css("background-color", "green");
+        opciones.hide();
+    });
+    $("#noListo-03").click(function(){
+        $("#titulo-03").css("background-color", "red");
+        opciones.hide();
+    });
+    $("#actualizar-03").click(function(){
+        $("#titulo-03").css("background-color", "blue");
+    });
+
 
     $("#titulo-04").click(function(){
         opciones.css("border-top", "none");
         $("#opciones-04").fadeToggle();
     });
+    $("#listo-04").click(function(){
+        $("#titulo-04").css("background-color", "green");
+        opciones.hide();
+    });
+    $("#noListo-04").click(function(){
+        $("#titulo-04").css("background-color", "red");
+        opciones.hide();
+    });
+    $("#actualizar-04").click(function(){
+        $("#titulo-04").css("background-color", "blue");
+    });
+
 
     $("#titulo-05").click(function(){
         opciones.css("border-top", "none");
         $("#opciones-05").fadeToggle();
     });
+    $("#listo-05").click(function(){
+        $("#titulo-05").css("background-color", "green");
+        opciones.hide();
+    });
+    $("#noListo-05").click(function(){
+        $("#titulo-05").css("background-color", "red");
+        opciones.hide();
+    });
+    $("#actualizar-05").click(function(){
+        $("#titulo-05").css("background-color", "blue");
+    });
+
 
     $("#titulo-06").click(function(){
         opciones.css("border-top", "none");
         $("#opciones-06").fadeToggle();
     });
+    $("#listo-06").click(function(){
+        $("#titulo-06").css("background-color", "green");
+        opciones.hide();
+    });
+    $("#noListo-06").click(function(){
+        $("#titulo-06").css("background-color", "red");
+        opciones.hide();
+    });
+    $("#actualizar-06").click(function(){
+        $("#titulo-06").css("background-color", "blue");
+    });
+
 
     $("#titulo-07").click(function(){
         opciones.css("border-top", "none");
         $("#opciones-07").fadeToggle();
     });
+    $("#listo-07").click(function(){
+        $("#titulo-07").css("background-color", "green");
+        opciones.hide();
+    });
+    $("#noListo-07").click(function(){
+        $("#titulo-07").css("background-color", "red");
+        opciones.hide();
+    });
+    $("#actualizar-07").click(function(){
+        $("#titulo-07").css("background-color", "blue");
+    });
+
 
     $("#titulo-08").click(function(){
         opciones.css("border-top", "none");
         $("#opciones-08").fadeToggle();
     });
+    $("#listo-08").click(function(){
+        $("#titulo-08").css("background-color", "green");
+        opciones.hide();
+    });
+    $("#noListo-08").click(function(){
+        $("#titulo-08").css("background-color", "red");
+        opciones.hide();
+    });
+    $("#actualizar-08").click(function(){
+        $("#titulo-08").css("background-color", "blue");
+    });
+
 
     $("#titulo-09").click(function(){
         opciones.css("border-top", "none");
         $("#opciones-09").fadeToggle();
     });
+    $("#listo-09").click(function(){
+        $("#titulo-09").css("background-color", "green");
+        opciones.hide();
+    });
+    $("#noListo-09").click(function(){
+        $("#titulo-09").css("background-color", "red");
+        opciones.hide();
+    });
+    $("#actualizar-09").click(function(){
+        $("#titulo-09").css("background-color", "blue");
+    });
+
 
 })
 
